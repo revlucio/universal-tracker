fc58484ba95fc2738d9f8c0f639ee12335cf8d8a
Luke Elliott <revlucio@gmail.com>
2015-10-20 09:44:31 +0100

    charts now allow selections

--- www/js/event/chart.controller.js
+++ www/js/event/chart.controller.js
@@ -1,8 +1,33 @@
 angular.module('tracker')
 	.controller('ChartCtrl', ChartCtrl);
 
-function ChartCtrl(oneSelfService) {
+function ChartCtrl(oneSelfService, $location, activityService, AuthenticationService) {
 	var vm = this;
 
-	window.open(oneSelfService.getChartUrl(["Coding"]), '_system', 'location=no');
+    vm.activities = activityService.getActivities();
+    vm.chart = {};
+    vm.showChart = showChart;
+
+    function init() {
+        if (!AuthenticationService.authenticated()) {
+            AuthenticationService.authenticate(true, function(res) {
+                if (!res) $location.path('/dash');
+            });
+        }
+    };
+    init();
+
+    function showChart() {
+        // var activity = vm.chart.type;
+        // var api_credentials = angular.fromJson(window.localStorage.api_credentials),
+        //     tags = ActivitiesService.getTags(activity),
+        //     uri = API.endpoint + "/v1/streams/" +
+        //     api_credentials.streamid + "/events/" +
+        //     tags.objectTags.join(',') + "/" +
+        //     tags.actionTags.join(',') +
+        //     "/sum(duration)/daily/barchart?readToken=" +
+        //     api_credentials.readToken;
+		var url = oneSelfService.getChartUrl([vm.chart.type])
+        window.open(url, '_system', 'location=no');
+    };
 }
