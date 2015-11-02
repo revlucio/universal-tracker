decf99bca907ceb0de272dd3bdff97f0088e7cfb
Luke Elliott <revlucio@gmail.com>
2015-11-02 15:50:06 +0000

    numeric inputs fixed for ios

--- www/js/app.module.js
+++ www/js/app.module.js
@@ -24,7 +24,6 @@ angular.module('tracker')
           // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
           // for form inputs)
           if (window.cordova && window.cordova.plugins.Keyboard) {
-
               //Change this to false to return accessory bar 
               cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
           }
